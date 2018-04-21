b22788569f777a04356de627f7fcdb0e53821113
张鹏 <zhangp@gionee.com>
2018-04-21 15:44:37 +0800

    springboot

---
+++ SpringBootDemo/src/main/java/com/roc/springboot/SpringBootDemo/FirstspringbootApplication.java
@@ -0,0 +1,15 @@
+package com.roc.springboot.SpringBootDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstspringbootApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		SpringApplication.run(FirstspringbootApplication.class, args);
+	}
+
+}
